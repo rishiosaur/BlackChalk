df9672eda94341ec79f41693536a1048088725ca
Rishi Kothari <itsrishikothari@gmail.com>
2019-11-13 11:34:40 -0500

    Stuff

--- introduction/index.py
+++ introduction/index.py
@@ -307,6 +307,18 @@ class Sum2(Scene):
         self.play(Transform(subtract, TexMobject(r"2S_2 - \frac{1}{2} = 0")))
         self.play(Transform(subtract, TexMobject(r"2S_2 = \frac{1}{2}")))
         self.play(Transform(subtract, TexMobject(r"S_2 = \frac{1}{4}")))
+
+class Sum3(Scene):
+    def construct(self):
+        # Now, we can get to the heart of the problem: figuring out what the sum of all natural numbers is equivalent to.
+        section = TextMobject("Section 1.4: The Third Sum")
+        self.play(Write(section))
+        self.wait(2)
+        self.play(FadeOutAndShiftDown(section))
+
+        # Just like the other sections, here's the sum.
+        eq_with_variable = TexMobject(r"S_3 = \displaystyle\sum_{n=0}^{\infty}n = 1+2+3+4+...")
+        self.play(Write(eq_with_variable))
         
 
 class IntroBanner(Scene):
@@ -349,36 +361,3 @@ class Logo(Scene):
 class wait(Scene):
     def construct(self):
         self.wait(1)
-
-
-class PlotFunctions(GraphScene):
-    CONFIG = {
-        "x_min": -10,
-        "x_max": 10,
-        "y_min": -1.5,
-        "y_max": 1.5,
-        "graph_origin": ORIGIN,
-        "function_color": RED,
-        "axes_color": GREEN,
-        "x_labeled_nums": range(-10, 12, 2),
-
-    }
-    def construct(self):
-        self.setup_axes(animate = True)
-        func_graph = self.get_graph(self.func_to_graph, self.function_color)
-        func_graph2 = self.get_graph(self.func_to_graph2, PURPLE)
-        vert_line = self.get_vertical_line_to_graph(TAU, func_graph, color = YELLOW)
-        graph_lab = self.get_graph_label(func_graph, label = "\\cos(x)")
-        graph_lab2 = self.get_graph_label(func_graph2, label = "\\sin(x)", x_val = -10, direction = UP / 2)
-        two_pi = TexMobject("x = 2 \\pi")
-        label_coord = self.input_to_graph_point(TAU, func_graph)
-        two_pi.next_to(label_coord, RIGHT + UP)
-
-        self.play(ShowCreation(func_graph), ShowCreation(func_graph2))
-        self.play(ShowCreation(vert_line), ShowCreation(graph_lab), ShowCreation(graph_lab2), ShowCreation(two_pi))
-
-    def func_to_graph(self, x):
-        return math.sqrt(1-x**2)
-
-    def func_to_graph2(self, x):
-        return np.sin(x)
